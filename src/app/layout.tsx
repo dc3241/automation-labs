19eaff30fbe9789a0179ebbd4f2e551c3610a4c5
dc3241 <d.ciccaglione1@gmail.com>
2025-08-20 19:16:36 -0700

    Update to modern Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import Footer from "../../components/Footer";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
